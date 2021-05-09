5a8a2f608f461c9317c59872fe9163aa68a5916d
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2021-05-09 16:05:16 +0000

    [Add]4-3画像投稿機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2bb666f8a3441680c8c44e07957bc49f7e20a9d9bf6891c59b18e81169aebf59adeef04130f831b88a6f2c43963a8fde84eed34c0805695b2cbced998a71f57d'
